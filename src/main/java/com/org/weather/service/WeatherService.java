6354f8b8b791f4ad02d9008ede7450a34c379a67
priyanjaygoswami <priyanjay.goswami150193@gmail.com>
2025-07-29 16:22:58 +0530

    get weather api

--- src/main/java/com/org/weather/service/WeatherService.java
+++ src/main/java/com/org/weather/service/WeatherService.java
@@ -2,7 +2,6 @@ package com.org.weather.service;
 
 import com.org.weather.model.WeatherResponse;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
